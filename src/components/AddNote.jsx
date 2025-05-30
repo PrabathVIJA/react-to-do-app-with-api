265208cdc890fef379377ca00313b8a6649376a6
prabathkiran <prabathkiran1@gmail.com>
2025-05-30 18:40:25 +0530

    remove type attribute

--- src/components/AddNote.jsx
+++ src/components/AddNote.jsx
@@ -3,7 +3,6 @@ export default function AddNote({ addNote, value }) {
     <>
       <div className="note-new">
         <textarea
-          type="number"
           rows="5"
           cols="30"
           placeholder="Please enter random id.."
