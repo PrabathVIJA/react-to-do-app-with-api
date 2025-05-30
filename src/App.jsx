6cdac2d4b2213fe0a8f7d787c76c385dc267a2f5
prabathkiran <prabathkiran1@gmail.com>
2025-05-30 14:20:47 +0530

    pushing branch

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,7 @@ function App() {
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/todos/7");
   }, []);
-  return (
-    <>
-      <h2>hi</h2>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
